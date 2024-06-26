e08cc5349962ed6943c2da1e309ae418b0787975
Issa SIDIBE <160803787+IMPERFact11@users.noreply.github.com>
2024-06-26 21:07:00 +0200

    Update Task 4: neural network.py

--- Task 4.py
+++ Task 4.py
@@ -1,3 +1,5 @@
+#ce code crée un réseau neuronal simple avec une fonction d'entrée qui augmente linéairement au fil du temps et une fonction de sortie qui affiche une valeur HTML indiquant si l'entrée est "Low" ou "High"
+en fonction de sa valeur. La couleur du texte change également en fonction de la valeur : rouge pour "Low" et vert pour "High"
 import nengo
 import numpy as np
 from nengo.processes import Piecewise 
